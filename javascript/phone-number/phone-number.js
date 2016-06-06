278f150d7bd84f73ec59e690025b4ff9286d2416
PenneyGadget <dreamofgreen@gmail.com>
2016-06-06 14:24:07 -0600

    Tests passing for phone number exercism in JS

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,9 +1,35 @@
-var PhoneNumber = function(number) {
-  this.number = number;
+var PhoneNumber = function(num) {
+  this.cleanNum = clean(num);
+  this.num = num;
 };
 
 PhoneNumber.prototype.number = function() {
-  return number;
+  return this.cleanNum;
 };
+
+PhoneNumber.prototype.areaCode = function() {
+  return this.num.slice(0, 3);
+};
+
+PhoneNumber.prototype.toString = function() {
+  if(this.num.length === 10) {
+    return "(" + this.num.slice(0, 3) + ") " +
+    this.num.slice(3, 6) + "-" + this.num.slice(6);
+  }
+};
+
+function clean(num) {
+  num = num.replace(/\D/g,'');
+
+  if(num[0] === '1' && num.length != 10) {
+    num = num.substr(1);
+  }
+
+  if(num.length != 10 || num[0] != '1') {
+    num = '0000000000';
+  }
+
+  return num;
+}
 
 module.exports = PhoneNumber;
---
+++ ruby/food-chain/food_chain.rb
@@ -0,0 +1,12 @@
+class FoodChain
+
+  def self.song
+    verse_one
+  end
+
+  def self.verse_one
+    "I know an old lady who swallowed a fly.\n
+     I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
+  end
+
+end
